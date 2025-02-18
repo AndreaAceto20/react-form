9bc56a0985f8bdd52ee22a04e31923b9c686cfbb
Andrea Aceto <andrea.aceto20@gmail.com>
2025-02-18 16:08:46 +0100

    [ADD]Milestone 2 (bug)

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -11,16 +11,30 @@ const InitialItem = ['uova', 'latte', 'pane', 'pasta']
 export default function List() {
 
     const [item, setItem] = useState(InitialItem);
+    const [NewItem, setNewItem] = useState('');
+
+    const addItem = e => {
+        e.preventDefault();
+        const updatedItem = [...item, NewItem];
+        setItem(updatedItem);
+        setNewItem('');
+    };
 
 
 
     return (
         <>
-            <p>{InitialItem.join(' ')}</p>
-            {/* <form>
-                <input type="text" value={NewItem} ></input>
 
-            </form> */}
+            <form onSubmit={addItem}>
+                <input type="text" value={NewItem} onChange={event => setNewItem(event.target.value)}></input>
+                <button>Invia</button>
+            </form>
+            <ul className='todolist'>
+                {item.map((item, i) => (
+                    <li key={i}>
+                        {item}
+                    </li>))}
+            </ul>
         </>
     )
 }
